c7d379afcf368dc0345393dd8521d9278e8d1019
pydick <p1053192517@163.com>
2025-09-22 17:34:07 +0800

    fix：优化格式化

--- .prettierrc.js
+++ .prettierrc.js
@@ -5,5 +5,25 @@ export default {
   bracketSpacing: true,
   singleQuote: false,
   arrowParens: "avoid",
-  trailingComma: "none"
+  trailingComma: "none",
+  
+  // 全局禁用单属性换行
+  singleAttributePerLine: false,
+  // 设置较大的打印宽度避免过早换行
+  printWidth: 120,
+  
+  // Vue文件特殊配置
+  overrides: [
+    {
+      files: "*.vue",
+      options: {
+        // Vue template中的HTML属性不换行
+        singleAttributePerLine: false,
+        // 设置较大的打印宽度
+        printWidth: 150,
+        // 保持HTML标签属性在同一行
+        htmlWhitespaceSensitivity: "ignore"
+      }
+    }
+  ]
 };
