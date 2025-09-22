3d8a037ccc55cce5d81ec68bab37fd5dfb7179e1
pydick <p1053192517@163.com>
2025-09-22 11:50:53 +0800

    pref :删除无用组件

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -2,7 +2,6 @@ import type { App } from "vue";
 import { setupAutoAnimate } from "./autoAnimate";
 import { setupDayjs } from "./dayjs";
 import { setupI18n } from "./i18n";
-import { setupPinia } from "./pinia";
 import { setupTanstackVueQuery } from "./tanstack-vue-query";
 
 export function setupPlugins(app: App) {
@@ -10,5 +9,4 @@ export function setupPlugins(app: App) {
   setupAutoAnimate(app);
   setupTanstackVueQuery(app);
   setupI18n(app);
-  setupPinia(app);
 }
