748e319fb424e844496032ddc7e07b5336d1abbc
pydick <p1053192517@163.com>
2025-09-26 17:14:15 +0800

    fix 回退drugflow i18n

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,8 +1,10 @@
 import type { App } from "vue";
 import { setupAutoAnimate } from "./autoAnimate";
 import { setupDayjs } from "./dayjs";
+import { setupI18n } from "./i18n";
 
 export function setupPlugins(app: App) {
   setupDayjs();
   setupAutoAnimate(app);
+  setupI18n(app);
 }
