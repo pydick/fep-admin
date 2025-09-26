7f23c4876a01db7ffc63c353dcf9ddf4b1d16536
pydick <p1053192517@163.com>
2025-09-26 15:15:38 +0800

    feat 移除drugflow i18n

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,10 +1,8 @@
 import type { App } from "vue";
 import { setupAutoAnimate } from "./autoAnimate";
 import { setupDayjs } from "./dayjs";
-import { setupI18n } from "./i18n";
 
 export function setupPlugins(app: App) {
   setupDayjs();
   setupAutoAnimate(app);
-  setupI18n(app);
 }
