41fce56b933564a97361436cfbeb89b95ceae7ab
pydick <p1053192517@163.com>
2025-10-11 17:53:37 +0800

    perf: 删除无用

--- src/views/welcome/index.vue
+++ src/views/welcome/index.vue
@@ -7,6 +7,6 @@ defineOptions({
 <template>
   <div>
     <h1>FEP Welcome</h1>
-    <el-button type="success">拥有code：'btn_add' 权限可见</el-button>
+    <!-- <el-button type="success">拥有code：'btn_add' 权限可见</el-button> -->
   </div>
 </template>
