c6d83d5ef4e816109cb9a50ba54fb9090b2786a4
pydick <p1053192517@163.com>
2025-10-16 13:04:02 +0800

    fix: 修改文案

--- src/views/welcome/index.vue
+++ src/views/welcome/index.vue
@@ -3,7 +3,7 @@ import { getToken, setToken } from "@/utils/auth";
 import Cookies from "js-cookie";
 import dayjs from "dayjs";
 import { getMe } from "@/api/user";
-
+import { ref, onMounted } from "vue";
 defineOptions({
   name: "Welcome"
 });
@@ -11,24 +11,27 @@ const handleClick = () => {
   let data = getToken();
   let deepData = JSON.parse(JSON.stringify(data));
   const newexpires = dayjs(data.expires).subtract(3, "hour").valueOf();
-  console.log(333, newexpires);
   data.expires = newexpires;
-  console.log(222, data);
   Cookies.set("authorized-token", JSON.stringify(data));
   const test = Cookies.get("authorized-token");
-  console.log(test);
+  expires.value = dayjs(JSON.parse(test).expires).format("YYYY-MM-DD HH:mm:ss");
 };
 const testGetMe = async () => {
   const res = await getMe({});
   console.log(res);
 };
+const expires = ref("");
+onMounted(() => {
+  expires.value = dayjs(getToken().expires).format("YYYY-MM-DD HH:mm:ss");
+});
 </script>
 
 <template>
   <div>
     <h1>FEP Welcome</h1>
-    <el-button @click="handleClick">更新过期时间</el-button>
-    <el-button @click="testGetMe">获取当前用户</el-button>
+    <el-button @click="handleClick">过期时间-3小时</el-button>
+    <el-button @click="testGetMe">在过期后,刷新token后获取当前用户</el-button>
+    过期时间:{{ expires }}
     <!-- <el-button type="success">拥有code：'btn_add' 权限可见</el-button> -->
   </div>
 </template>
