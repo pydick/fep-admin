9b9092c7c7a1b8c73febdf6d0e3006ba65a55832
pydick <p1053192517@163.com>
2025-10-23 09:47:31 +0800

    feat: mock 数据调整

--- mock/test.ts
+++ mock/test.ts
@@ -2,7 +2,44 @@ import { defineFakeRoute } from "vite-plugin-fake-server/client";
 import { proteinsData } from "./proteinsData.js";
 export default defineFakeRoute([
   {
-    url: "/api/v1/oss/list",
+    url: "/api/v1/auth/login1",
+    method: "post",
+    response: ({ body }) => {
+      if (body.username === "pengyong") {
+        return {
+          success: true,
+          data: {
+            avatar: "https://avatars.githubusercontent.com/u/44761321",
+            username: "admin",
+            nickname: "小铭",
+            // 一个用户可能有多个角色
+            roles: ["admin"],
+            // 按钮级别权限
+            permissions: ["*:*:*"],
+            accessToken: "eyJhbGciOiJIUzUxMiJ9.admin",
+            refreshToken: "eyJhbGciOiJIUzUxMiJ9.adminRefresh",
+            expires: "2030/10/30 00:00:00"
+          }
+        };
+      } else {
+        return {
+          success: true,
+          data: {
+            avatar: "https://avatars.githubusercontent.com/u/52823142",
+            username: "common",
+            nickname: "小林",
+            roles: ["common"],
+            permissions: ["permission:btn:add", "permission:btn:edit"],
+            accessToken: "eyJhbGciOiJIUzUxMiJ9.common",
+            refreshToken: "eyJhbGciOiJIUzUxMiJ9.commonRefresh",
+            expires: "2030/10/30 00:00:00"
+          }
+        };
+      }
+    }
+  },
+  {
+    url: "/api/v1/oss/list1",
     method: "get",
     response: ({ body }) => {
       return {
@@ -16,7 +53,7 @@ export default defineFakeRoute([
     }
   },
   {
-    url: "/api/v1/oss/download",
+    url: "/api/v1/oss/download1",
     method: "get",
     response: ({ body }) => {
       return {
@@ -29,7 +66,7 @@ export default defineFakeRoute([
     }
   },
   {
-    url: "/api/v1/protein/info/upload",
+    url: "/api/v1/protein/info/upload1",
     method: "post",
     response: ({ body }) => {
       return {
