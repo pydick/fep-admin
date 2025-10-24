a3bafd260b84cf918d2bff2abf02ec3f298b8ac9
pydick <p1053192517@163.com>
2025-10-24 16:10:52 +0800

    fix: 修复桶参数问题

--- src/api/fep.js
+++ src/api/fep.js
@@ -1,6 +1,6 @@
 import { http as request } from "@/utils/http";
 import { apiV1 } from "@/config/api";
-import { ossBucket } from "@/config/api";
+import { ossBucket as bucket } from "@/config/api";
 
 export function ossPostDownload(data) {
   return request.post(`${apiV1}/oss/download`, { data });
@@ -8,8 +8,8 @@ export function ossPostDownload(data) {
 
 export function ossUpload(data) {
   return request.post(
-    `${apiV1}/oss/upload`,
-    { bucket: ossBucket, ...data },
+    `${apiV1}/oss/upload?bucket=${bucket}`,
+    { data },
     {
       headers: { "content-type": "multipart/form-data" }
     }
@@ -17,11 +17,11 @@ export function ossUpload(data) {
 }
 
 export function ossGetDownload(params) {
-  return request.get(`${apiV1}/oss/download`, { bucket: ossBucket, ...params });
+  return request.get(`${apiV1}/oss/download`, { params: { bucket, ...params } });
 }
 
 export function ossList(params) {
-  return request.get(`${apiV1}/oss/list`, { bucket: ossBucket, ...params });
+  return request.get(`${apiV1}/oss/list`, { params: { bucket, ...params } });
 }
 
 export function proteinInfo(data) {
