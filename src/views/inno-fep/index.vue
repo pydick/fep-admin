d38bb758ec795d5624d59ebc9a547feb08944283
pydick <p1053192517@163.com>
2025-10-28 17:39:10 +0800

    feat: 添加静态页面

--- src/views/inno-fep/index.vue
+++ src/views/inno-fep/index.vue
@@ -4,15 +4,16 @@ import CScard from "@/components/CScard/index.vue";
 import CStab from "@/components/CStab/index.vue";
 import CreateTask from "./CreateTask/index.vue";
 import RecentResult from "./RecentResult/index.vue";
-import { ref } from "vue";
+import { ref, reactive } from "vue";
 defineOptions({
   // name 作为一种规范最好必须写上并且和路由的name保持一致
   name: "Inno-Fep"
 });
-const tabList = ref<{ label: string; name: string }[]>([
+const tabList = reactive<{ label: string; name: string }[]>([
   { label: "创建任务", name: "createTask" },
   { label: "最近结果", name: "recentResult" }
 ]);
+const activeName = ref(tabList[1].name);
 const handleTabClick = (name: string) => {
   console.log(name);
 };
@@ -24,7 +25,7 @@ const handleTabClick = (name: string) => {
       <FepHeader />
     </template>
     <template #content>
-      <CStab :tabList="tabList" @tabClcik="handleTabClick">
+      <CStab v-model:activeName="activeName" :tabList="tabList" @tabClcik="handleTabClick">
         <template #createTask>
           <CreateTask />
         </template>
