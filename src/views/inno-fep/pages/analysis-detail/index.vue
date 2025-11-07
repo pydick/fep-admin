6e851c6b3f981bc4c15c6d61bcd071a2044d2712
pydick <p1053192517@163.com>
2025-11-07 15:56:16 +0800

    feat: 添加mock数据

--- src/views/inno-fep/pages/analysis-detail/index.vue
+++ src/views/inno-fep/pages/analysis-detail/index.vue
@@ -2,6 +2,13 @@
 import one from "./charts/one.vue";
 import { mocksvg } from "./data";
 import { ref, computed } from "vue";
+import testImage from "@/assets/mockImg/test.png";
+import chart1 from "@/assets/mockImg/chart1.png";
+import chart2 from "@/assets/mockImg/chart2.png";
+import chart3 from "@/assets/mockImg/chart3.png";
+import chart4 from "@/assets/mockImg/chart4.png";
+import chart5 from "@/assets/mockImg/chart5.png";
+
 const structure = ref(mocksvg);
 // 将 SVG 字符串转换为 Data URL
 const structurePreviewUrl = computed(() => {
@@ -26,19 +33,19 @@ defineOptions({
 const tableData = ref([
   {
     legs: "Solvated",
-    conergence: mocksvg,
-    overlapMatrix: mocksvg,
-    replicaDistribution: mocksvg,
-    rmsf: mocksvg,
-    rotatableBonds: mocksvg
+    conergence: chart1,
+    overlapMatrix: chart2,
+    replicaDistribution: chart3,
+    rmsf: chart4,
+    rotatableBonds: chart5
   },
   {
     legs: "Complex",
-    conergence: mocksvg,
-    overlapMatrix: mocksvg,
-    replicaDistribution: mocksvg,
-    rmsf: mocksvg,
-    rotatableBonds: mocksvg
+    conergence: chart1,
+    overlapMatrix: chart2,
+    replicaDistribution: chart3,
+    rmsf: chart4,
+    rotatableBonds: chart5
   }
 ]);
 
@@ -86,7 +93,8 @@ const columns = ref([
   <div class="analysis-detail-container w-full h-full p-[15px]">
     <el-descriptions title="Pairs Info" direction="vertical" border>
       <el-descriptions-item :rowspan="3" :width="600" label="Pairs图" align="center">
-        <el-image :src="structureDataUrl" :preview-src-list="[structureDataUrl]" fit="contain" class="structure-container" :preview-teleported="true" />
+        <!-- <el-image :src="structureDataUrl" :preview-src-list="[structureDataUrl]" fit="contain" class="structure-container" :preview-teleported="true" /> -->
+        <el-image :src="testImage" :preview-src-list="[testImage]" fit="contain" class="structure-container" :preview-teleported="true" />
       </el-descriptions-item>
       <el-descriptions-item label="置信度">Bad</el-descriptions-item>
       <el-descriptions-item label="状态">success</el-descriptions-item>
@@ -94,22 +102,27 @@ const columns = ref([
       <el-descriptions-item label="Mapping score">0.321</el-descriptions-item>
       <el-descriptions-item label="Predicted ddG (Raw)">2.35±0.016</el-descriptions-item>
       <el-descriptions-item label="Predicted ddG (Corrected)">2.12±0.012</el-descriptions-item>
-      <el-descriptions-item label="对比图表">
+      <el-descriptions-item label="对比图表" class-name="comparison-table">
         <pure-table :data="tableData" :columns="columns" border>
           <template #conergence="{ row }">
-            <el-image :src="svgToDataUrl(row.conergence)" :preview-src-list="[svgToDataUrl(row.conergence)]" fit="contain" class="w-full charts-photo" :preview-teleported="true" />
+            <!-- <el-image :src="svgToDataUrl(row.conergence)" :preview-src-list="[svgToDataUrl(row.conergence)]" fit="contain" class="w-full charts-photo" :preview-teleported="true" /> -->
+            <el-image :src="row.conergence" :preview-src-list="[row.conergence]" fit="contain" class="w-full charts-photo" :preview-teleported="true" />
           </template>
           <template #overlapMatrix="{ row }">
-            <div class="w-full charts-photo" v-html="row.overlapMatrix" />
+            <!-- <div class="w-full charts-photo" v-html="row.overlapMatrix" /> -->
+            <el-image :src="row.overlapMatrix" :preview-src-list="[row.overlapMatrix]" fit="contain" class="w-full charts-photo" :preview-teleported="true" />
           </template>
           <template #replicaDistribution="{ row }">
-            <div class="w-full charts-photo" v-html="row.replicaDistribution" />
+            <!-- <div class="w-full charts-photo" v-html="row.replicaDistribution" /> -->
+            <el-image :src="row.replicaDistribution" :preview-src-list="[row.replicaDistribution]" fit="contain" class="w-full charts-photo" :preview-teleported="true" />
           </template>
           <template #rmsf="{ row }">
-            <div class="w-full charts-photo" v-html="row.rmsf" />
+            <!-- <div class="w-full charts-photo" v-html="row.rmsf" /> -->
+            <el-image :src="row.rmsf" :preview-src-list="[row.rmsf]" fit="contain" class="w-full charts-photo" :preview-teleported="true" />
           </template>
           <template #rotatableBonds="{ row }">
-            <div class="w-full charts-photo" v-html="row.rotatableBonds" />
+            <!-- <div class="w-full charts-photo" v-html="row.rotatableBonds" /> -->
+            <el-image :src="row.rotatableBonds" :preview-src-list="[row.rotatableBonds]" fit="contain" class="w-full charts-photo" :preview-teleported="true" />
           </template>
         </pure-table>
       </el-descriptions-item>
@@ -173,5 +186,10 @@ const columns = ref([
   .el-table__body tr:hover > td {
     background-color: transparent !important;
   }
+}
+
+:deep(.comparison-table) {
+  height: 100%;
+  padding: 15px !important;
 }
 </style>
