9f37f5ae851f05b1c907374a8c02d5da3d2d31ea
pydick <p1053192517@163.com>
2025-11-12 18:01:35 +0800

    perf: ui

--- src/views/inno-fep/pages/home/CreateTask/LigandPreprocess/index.vue
+++ src/views/inno-fep/pages/home/CreateTask/LigandPreprocess/index.vue
@@ -320,9 +320,9 @@ onMounted(async () => {
       <div class="pt-[15px] xl:pl-[100PX] 2xl:pl-0 flex-bc">
         <el-button type="primary" :disabled="buildDisabled" @click="handleGenerateMap">Build</el-button>
         <el-tooltip content="至少两个配体以上， Align后或者关闭分子叠合，才能去Build映射图" placement="top">
-          <el-icon class="ml-[5px] icon-common"><QuestionFilled /></el-icon>
+          <el-icon class="ml-[5px] icon-common mr-[15px]"><QuestionFilled /></el-icon>
         </el-tooltip>
-        <el-button :disabled="!isGernerate" @click="perturbationGraphShow">配体微扰图</el-button>
+        <el-button type="primary" :disabled="!isGernerate" @click="perturbationGraphShow">配体微扰图</el-button>
         <el-tooltip content="生成后，才能打开配体微扰图" placement="top">
           <el-icon class="ml-[5px] icon-common"><QuestionFilled /></el-icon>
         </el-tooltip>
