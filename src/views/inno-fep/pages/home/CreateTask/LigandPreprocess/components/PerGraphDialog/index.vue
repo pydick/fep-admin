7eea39745b58c2daedafd842aab2cbe40db20c2b
pydick <p1053192517@163.com>
2025-12-05 15:17:20 +0800

    fix:去除取消

--- src/views/inno-fep/pages/home/CreateTask/LigandPreprocess/components/PerGraphDialog/index.vue
+++ src/views/inno-fep/pages/home/CreateTask/LigandPreprocess/components/PerGraphDialog/index.vue
@@ -118,7 +118,7 @@ const handleEdgeChange = (value: any) => {
     </el-row>
     <template #footer>
       <div>
-        <el-button @click="handleCancel">取消</el-button>
+        <!-- <el-button @click="handleCancel">取消</el-button> -->
         <el-button type="primary" @click="handleSure">确定</el-button>
       </div>
     </template>
